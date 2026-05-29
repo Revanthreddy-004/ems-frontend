6e7573183f150f4aa7b8099b4a6451c563756930
Baddam Revanth Reddy <revanthbaddam919@gmail.com>
2026-05-29 20:57:00 +0530

    connected frontend to production backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://ems-backend-l992.onrender.com.api"
+  baseURL: "https://ems-backend-ukf6.onrender.com/api"
 });
 
 export default API;
